e2b3761ccfd852e18829f929b8ca815ab00199a5
shreeyashree-65 <1ds22cb045@dsce.edu.in>
2025-09-27 23:56:22 +0530

    Model embedding

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Medical-bot",
+    version="0.1.0",
+    author="Shreeya",
+    author_email="shreeyashreeyaps@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
